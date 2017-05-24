74b677c26617d11c6b96978b41e93cf5248011dc
crimson-med <mederic.burlet@gmail.com>
2017-05-24 10:09:03 +0200

    clean web

--- SmiteFR API/V_Information.vb
+++
@@ -1,6 +0,0 @@
-﻿'This Form just displays basic information about me :D
-Public Class V_Information
-    Private Sub V_Information_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
-    End Sub
-End Class
